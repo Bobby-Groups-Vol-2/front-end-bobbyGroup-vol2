96e21dc79ebafba322fd98c32882da66e10e9515
Meawmfia <boon20002543@hotmail.com>
2021-09-03 22:32:15 +0700

    [ADD] default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative min-h-screen">
+    <Nuxt class="relative top-10vh" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+html {
+  background-color: #00a4b7;
+  background-image: linear-gradient(#00a4b7, #7eb58b, #0b3939);
+}
+</style>
