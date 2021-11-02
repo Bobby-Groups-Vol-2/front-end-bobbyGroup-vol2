308763e22cccc6fb206b61002f4671d96444212d
padnitri <57793978+padnitri@users.noreply.github.com>
2021-11-02 21:17:11 +0700

    [Update] Remove psudo Login Navbar, link real login button

---
+++ pages/order/order.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <div class="bg-white text-black">
+          <h1 class=""> This is Order Page</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
