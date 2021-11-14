ca9946b507214f36ef9b340ccf7c81887e14420c
padnitri <57793978+padnitri@users.noreply.github.com>
2021-11-15 00:08:17 +0700

    [Update] Order Page Template not done

--- pages/order/order.vue
+++ pages/order/order.vue
@@ -1,8 +1,50 @@
 <template>
-  <div>
-      <div class="bg-white text-black">
-          <h1 class=""> This is Order Page</h1>
+  <div class="flex justify-center">
+    <div
+      class="
+        bg-white
+        w-11/12
+        mt-12
+        rounded-4xl
+        p-5
+        text-black
+        h-auto
+        shadow-2xl
+        sm:w-2/3
+      "
+    >
+      <div class="flex justify-center">
+        <div>
+          <img src="../../asset/user.png" class="w-1/2" />
+
+          <!-- User Data -->
+          <h2 class="text-lg">Username</h2>
+          <h2 class="text-md">Email</h2>
+        </div>
       </div>
+
+      <div>
+        <h1 class="mt-10">Your Orders</h1>
+        <hr />
+
+        <!-- Div Loop -->
+        <div>
+          <div
+            id="timing"
+            class="bg-red-700 flex justify-between text-white text-sm p-2"
+          >
+            <span> Date </span>
+            <span> Time </span>
+            <span> Order ID</span>
+          </div>
+          <div class="flex flex-col md:grid grid-cols-3 md:gap-12">
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +52,9 @@
 </script>
 
 <style>
+hr {
+  height: 2px;
+  background-color: black;
+  margin-bottom: 2%;
+}
 </style>
