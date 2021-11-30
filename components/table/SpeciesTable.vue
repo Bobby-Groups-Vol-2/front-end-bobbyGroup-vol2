677a4299c3b3a71bd010573a7e47a422edef250c
padnitri <57793978+padnitri@users.noreply.github.com>
2021-11-30 17:20:07 +0700

    [Update] SpeciesTable - Undone:Table

--- components/table/SpeciesTable.vue
+++ components/table/SpeciesTable.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="all-card-holder w-full flex justify-center">
+  <div class="all-card-holder w-full flex justify-center flex-col">
        <div class="flex flex-col my-10">
       <h1 class="mx-6 mb-4 font-bold text-2xl">Add Species</h1>
-      <add-species class="card lg:w-2/3"></add-species>
+      <add-species class="card"></add-species>
       <h1 class="mx-6 mb-4 font-bold text-2xl my-10">Add Pattern</h1>
-      <add-pattern class="card lg:w-2/3 "></add-pattern>
-      <h1 class="mx-6 mb-4 font-bold text-2xl my-10">Add Pattern <br/> to Species</h1>
-      <specis-patterns class="card lg:w-2/3 "></specis-patterns>
+      <add-pattern class="card "></add-pattern>
+      <h1 class="mx-6 mb-4 font-bold text-2xl my-10">Add Pattern to Species</h1>
+      <specis-patterns class="card "></specis-patterns>
     </div>
 
-    <!-- <table class="m-5 w-4/5 mx-auto bg-white text-gray-800">
+    <table class="m-5 w-4/5 mx-auto bg-white text-gray-800">
       <tr class="text-left border-b-2 border-gray-300">
         <th class="px-2 py-3">Species ID</th>
         <th class="px-2 py-3">Species Name</th>
@@ -51,7 +51,7 @@
           </button>
         </td>
       </tr>
-    </table> -->
+    </table>
   </div>
 </template>
 
@@ -102,8 +102,7 @@ h1 {
 }
 @media (min-width: 1024px) {
   .all-card-holder {
-    width: 100%;
-    justify-self: center;
+    width: 70%;
   }
 }
 </style>
